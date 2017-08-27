c6c41a3a35ae88395d9f4530eed961da1c27efef
Jacob <Jacob@JACOB-PC>
2017-08-26 22:34:09 -0500

    Updated to .Net Core 2.0.

--- TestWebsite/Program.cs
+++ TestWebsite/Program.cs
@@ -1,25 +1,42 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace TestWebsite
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+    //https://docs.microsoft.com/en-us/aspnet/core/migration/1x-to-2x/
+    //public static void Main(string[] args)
+    //{
+    //    var host = new WebHostBuilder()
+    //        .UseKestrel()
+    //        .UseContentRoot(Directory.GetCurrentDirectory())
+    //        .UseIISIntegration()
+    //        .UseStartup<Startup>()
+    //        .UseApplicationInsights()
+    //        .Build();
 
-            host.Run();
-        }
+    //    host.Run();
+    //}
+
+
+    public static void Main(string[] args)
+    {
+      BuildWebHost(args).Run();
     }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .UseApplicationInsights()
+            .Build();
+  }
 }
